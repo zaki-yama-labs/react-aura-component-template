d4309e17a6e110d8a6b8503e5ec55b3b9dec6022
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2016-06-22 17:27:36 +0900

    SLDSもwebpackに組み込む

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Button } from 'react-lightning-design-system';
+
+import '../stylesheets/index.scss';
 
 export const init = function(el, service) {
   render((
-    <div>Hello, World!</div>
+    <Button type="brand">foo</Button>
   ), el);
 };
